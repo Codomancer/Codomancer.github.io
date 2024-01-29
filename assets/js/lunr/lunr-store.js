e582c4e3119bedc1543249d250fb8fb292cd023a
Codomancer <152785084+Codomancer@users.noreply.github.com>
2024-01-29 22:02:31 +0100

    First article: SUDS Pro C++ integration

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Integrating SUDS Pro using C++",
+        "excerpt":"I have recently discovered the SUDS Pro dialogue system and was amazed by how many of my concerns regarding game dialogue it resolves. Not only that, SUDS Pro also takes care of a lot of related tasks that I did not even think of yet, such as triggering camera shake...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/SudsProIntegration",
+        "teaser": null
+      }]
