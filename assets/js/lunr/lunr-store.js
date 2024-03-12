b0221ef8bb2e108962014dffec88f2bcc38dabb2
Codomancer <152785084+Codomancer@users.noreply.github.com>
2024-03-12 22:02:56 +0100

    Add header image to post and thumbnail to post previw on the landing page

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,5 +3,5 @@ var store = [{
         "excerpt":"I have recently discovered the SUDS Pro dialogue system and was amazed by how many of my concerns regarding game dialogue it resolves. Not only that, SUDS Pro also takes care of a lot of related tasks that I did not even think of yet, such as triggering camera shake...","categories": ["jekyll","update"],
         "tags": [],
         "url": "/SudsProIntegration",
-        "teaser": null
+        "teaser": "/assets/images/SudsProCppUi/ThumbnailSudsProCppInterface.jpg"
       }]
